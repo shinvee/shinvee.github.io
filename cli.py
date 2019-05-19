0782f19ddd76e19b3c8cb677885a03a3889ac8f8
JC Kim <jck@linecorp.com>
2019-05-19 12:06:32 +0900

    refactor: 💡 extract block_path to make code testable

--- cli.py
+++ cli.py
@@ -7,14 +7,14 @@ import frontmatter
 from jinja2 import Template
 
 
-def get_last_block():
-    blocks = os.listdir('blocks')
+def get_last_block(block_path='blocks'):
+    blocks = os.listdir(block_path)
     if len(blocks) == 0:
         return None
     else:
         linked = []
         for block_hash in blocks:
-            block = frontmatter.load(f'blocks/{block_hash}')
+            block = frontmatter.load(f'{block_path}/{block_hash}')
             if 'prev' in block:
                 linked.append(block['prev'])
         return [i for i in blocks if i not in linked][0]
@@ -26,7 +26,8 @@ def get_last_block():
               prompt='Message')
 def create_block(message):
     meta = dict()
-    last_block = get_last_block()
+    block_path = 'blocks'
+    last_block = get_last_block(block_path)
     if last_block:
         meta['prev'] = last_block
     meta['created_at'] = datetime.datetime.now().isoformat()
@@ -38,7 +39,7 @@ def create_block(message):
         block_hash = hashlib.sha256(data.encode()).hexdigest()
         if block_hash[:2] == '00':
             break
-    with open(f'blocks/{block_hash}', 'w') as block_file:
+    with open(f'{block_path}/{block_hash}', 'w') as block_file:
         print(data, file=block_file)
         print(data)
 
