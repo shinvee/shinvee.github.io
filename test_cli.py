e6a8508c4a727ccdea2791a93567ad07855b430a
JC Kim <jck@linecorp.com>
2019-05-27 08:51:05 +0900

    test: add create_block test

--- test_cli.py
+++ test_cli.py
@@ -1,6 +1,12 @@
-from cli import get_last_block_hash
+from cli import get_last_block_hash, create_block
 
 
 def test_get_last_block_hash():
     last = get_last_block_hash('test_blocks')
     assert last == '00517baba6aed0172711aa9d6d28096378ae1a847e22a8c2829fc0a6523830ab' # noqa
+
+
+def test_create_blocvk():
+    block, block_hash = create_block('test', 'test_blocks')
+    assert block['prev'] == get_last_block_hash('test_blocks')
+    assert block.content == 'test'
